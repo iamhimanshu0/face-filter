9018b03071afd8693a2473774585e7d3f34ceecf
himanshu <iamhimanshu0>
2021-12-17 13:17:36 +0530

    adding check for phone or laptop

--- detection.js
+++ detection.js
@@ -27,3 +27,22 @@ const camera = new Camera(videoElement, {
   height: 480,
 });
 camera.start();
+
+/* Storing user's device details in a variable*/
+let details = navigator.userAgent;
+
+/* Creating a regular expression 
+        containing some mobile devices keywords 
+        to search it in details string*/
+let regexp = /android|iphone|kindle|ipad/i;
+
+/* Using test() method to search regexp in details
+        it returns boolean value*/
+let isMobileDevice = regexp.test(details);
+
+if (isMobileDevice) {
+  alert("Please use a Desktop/Laptop in order to run the filter");
+  window.location = "https://christmasfilter.netlify.app/";
+} else {
+  console.log("You are using Desktop then its okey");
+}
